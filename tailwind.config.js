1938ef9b53f721af129b80717fe79883320e5b20
Aliyaser <aliyasermousavi@gmail.com>
2025-06-15 16:00:20 +0430

    fix (main) : fix error

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+MediaSourceHandle.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode : 'class',
+  theme : {
+    extend: {},
+  },
+  plugins: [],
+}
